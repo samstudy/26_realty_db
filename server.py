316132d5828f43d8466318d4de2b52efbf99ade9
sam156 <bskordo@gmail.com>
2017-06-21 05:10:04 -0400

    Added ip

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ from app_db_init import app
 from db_model import Flats
 
 
-
 @app.route("/")
 def ads_list(page=1):
     post_per_page = 10
-    ads = Flats.query.filter(Flats.is_expired==False).paginate(page, post_per_page, False)
+    ads = Flats.query.filter(Flats.is_expired==False).paginate(page,
+                                                               post_per_page, False)
     return render_template('ads_list.html', ads=ads)
 
 
@@ -16,19 +16,21 @@ def search(page=1):
     session['dist'] = request.form['oblast_district']
     session['minv'] = request.form['min_price'] 
     session['maxv'] = request.form['max_price']
-    session['new_building'] = request.args.get('new_building', True, type=bool)
+    session['new_building'] = request.args.get('new_building',
+                                                True, type=bool)
     return redirect(url_for('results'))
 
 
 @app.route('/results', methods=['GET'])
 def results(page=1):
     post_per_page = 10
-    ads = Flats.query.filter(Flats.oblast_district == session['dist'],Flats.is_expired == False, 
-          Flats.price > session['minv'], Flats.price < session['maxv'],
-          Flats.under_construction == session['new_building']).paginate(page, post_per_page, False)
+    ads = Flats.query.filter(Flats.oblast_district == session['dist'],
+                             Flats.is_expired == False, Flats.price > session['minv'],
+                             Flats.price < session['maxv'],
+                             Flats.under_construction == session['new_building']).paginate(page, post_per_page, False)
 
     return render_template('ads_list.html', ads=ads)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
