fce31408ebcaba6651bd1e7a0cd5acd9e3ee7f63
Samat Karymshakov <bskordo@gmail.com>
2017-11-12 22:17:38 +0600

    Add the code to find out db URI

--- app_db_init.py
+++ app_db_init.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///media/sf_Projects/26_realty_dbassets.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), 'assets.db')
 app.secret_key = 'secret_key'
 db = SQLAlchemy(app)
