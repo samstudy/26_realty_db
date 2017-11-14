88eef348c75274e19341a5f801bf73476898dada
sam156 <bskordo@gmail.com>
2017-11-14 01:51:52 -0500

    Added updated function

--- insert_into_db.py
+++ insert_into_db.py
@@ -2,8 +2,9 @@ import argparse
 import sys
 import os
 import json
-from app_db_init import app, db
+from ap_db_init import app, db
 from db_model import Flats
+from sqlalchemy import update
 
 
 def get_args():
@@ -20,13 +21,31 @@ def load_data(filepath):
 
 
 def insert_into_db_data(apartments):
-    for ap in apartments:
-        room = Flats(ap['id'], ap['has_balcony'], ap['oblast_district'], 
-                     ap['construction_year'], ap['description'], ap['settlement'],
-                     ap['rooms_number'],ap['living_area'], ap['address'], ap['price'],
-                     ap['premise_area'],ap['under_construction'], ap['is_expired'])
-        db.session.add(room)
-        db.session.commit()
+    for apartment in apartments:
+        updated_flats = Flats.query.filter_by(room_id = apartment['id']).first()
+        if updated_flats:
+            updated_flats.has_balcony = apartment['has_balcony'],
+            updated_flats.oblast_district = apartment['oblast_district'],
+            updated_flats.construction_year = apartment['construction_year'],
+            updated_flats.description = apartment['description'],
+            updated_flats.settlement = apartment['settlement'],
+            updated_flats.rooms_number = apartment['rooms_number'],
+            updated_flats.living_area = apartment['living_area'],
+            updated_flats.address = apartment['address'],
+            updated_flats.price = apartment['price'],
+            updated_flats.premise_area = apartment['premise_area'],
+            updated_flats.under_construction = apartment['under_construction']
+            db.session.commit()
+        else:
+
+            room = Flats(apartment['id'], apartment['has_balcony'],
+                         apartment['oblast_district'],apartment['construction_year'],
+                         apartment['description'], apartment['settlement'],
+                         apartment['rooms_number'],apartment['living_area'],
+                         apartment['address'], apartment['price'],apartment['premise_area'],
+                         apartment['under_construction'],apartment['is_expired'])
+            db.session.add(room)
+            db.session.commit()
 
 
 if __name__ == '__main__':
