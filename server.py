7c113570daa8c5a3959b6739abc8e136be92642c
sam156 <bskordo@gmail.com>
2017-11-17 11:37:23 -0500

    Made one basic function instead a few

--- server.py
+++ server.py
@@ -3,34 +3,27 @@ from app_db_init import app
 from db_model import Flats
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def ads_list(page=1):
     post_per_page = 10
-    ads = Flats.query.filter(Flats.is_expired==False).paginate(page,
-                                                               post_per_page, False)
-    return render_template('ads_list.html', ads=ads)
-
-
-@app.route('/search', methods=['POST'])
-def search(page=1):
-    session['dist'] = request.form['oblast_district']
-    session['minv'] = request.form['min_price'] 
-    session['maxv'] = request.form['max_price']
-    session['new_building'] = request.args.get('new_building',
-                                                True, type=bool)
-    return redirect(url_for('results'))
-
-
-@app.route('/results', methods=['GET'])
-def results(page=1):
-    post_per_page = 10
-    ads = Flats.query.filter(Flats.oblast_district == session['dist'],
-                             Flats.is_expired == False, Flats.price > session['minv'],
-                             Flats.price < session['maxv'],
-                             Flats.under_construction == session['new_building']).paginate(page, post_per_page, False)
-
-    return render_template('ads_list.html', ads=ads)
-
+    ads = Flats.query.filter(Flats.is_expired == False)
+    oblast_district = request.args.get('oblast_district', default=None, type=None)
+    min_price = request.args.get('min_price', default=None, type=None)
+    max_price = request.args.get('max_price', default=None, type=None)
+    under_construction = request.args.get('new_building', True, type=bool)
+    if oblast_district:
+        ads = ads.filter(Flats.oblast_district == oblast_district)
+    if min_price:
+        ads = ads.filter(Flats.price > min_price)
+    if max_price:
+        ads = ads.filter(Flats.price < max_price)
+    if under_construction:
+        ads = ads.filter(Flats.under_construction == under_construction)
+    return render_template('ads_list.html',
+                            ads=ads.paginate(page, post_per_page, False),
+                            min_price = min_price,
+                            max_price= max_price,
+                            oblast_district = oblast_district)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
