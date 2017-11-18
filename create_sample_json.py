9dc44e5d834629e9e315df1f431dc3124f2d2ad8
Samat Karymshakov <bskordo@gmail.com>
2017-11-18 11:05:35 +0600

    Rename the value,added space

--- create_sample_json.py
+++ create_sample_json.py
@@ -8,9 +8,9 @@ def create_sample_file():
     url = 'https://devman.org/media/filer_public/e5/62/e56287d2-9519-4e18-878a-6d4849b628e2/ads.json'
     response = requests.get(url)    
     flats = response.json()
-    result = [dict(flat, **{'is_expired':random.choice([True, False])}) for flat in flats]
+    json_content = [dict(flat, **{'is_expired':random.choice([True, False])}) for flat in flats]
     with open('sample.json', 'w') as file_handler:
-       json.dump(result, file_handler, ensure_ascii=False)
+        json.dump(json_content, file_handler, ensure_ascii=False)
 
 if __name__ == '__main__':
-	create_sample_file()
+    create_sample_file()
